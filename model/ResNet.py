959f95e31f274bb444e24074d2a19b39641666ef
gaga <wangzidongprince@163.com>
2023-03-03 01:47:10 +0800

    feat: resnet34 trained

--- model/ResNet.py
+++ model/ResNet.py
@@ -7,7 +7,7 @@ def conv3x3(input_chanels, output_chanels, stride=1, padding=1):
 
 
 def conv1x1(input_chanels, output_chanels, stride=1, padding=1):
-    return nn.Conv2d(input_chanels, output_chanels, kernel_size=1, stride=stride, padding=1)
+    return nn.Conv2d(input_chanels, output_chanels, kernel_size=1, stride=stride, padding=padding)
 
 
 class BasicBlock(nn.Module):
@@ -17,11 +17,11 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         if norm_Layer is None:
             norm_Layer = nn.BatchNorm2d
-        self.conv1 = conv3x3(input_chanels, output_chanels, stride)
+        self.conv1 = conv3x3(input_chanels, output_chanels, stride, padding=1)
         self.bn1 = norm_Layer(output_chanels)
         self.relu = nn.ReLU(inplace=True)
 
-        self.conv2 = conv3x3(output_chanels, output_chanels, stride)
+        self.conv2 = conv3x3(output_chanels, output_chanels, 1, padding=1)
         self.bn2 = norm_Layer(output_chanels)
 
         self.downsample = downsample
@@ -35,8 +35,15 @@ class BasicBlock(nn.Module):
         out = self.conv2(out)
         out = self.bn2(out)
 
+        # print('idemtity shape pre', identity.shape)
+        # print('self.downsample', self.downsample)
+
         if self.downsample:
             identity = self.downsample(x)
+
+        # print('self.stride', self.stride)
+        # print('out shape', out.shape)
+        # print('idemtity shape', identity.shape)
 
         out += identity
         out = self.relu(out)
@@ -47,9 +54,9 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, input_chanels, output_chanels, stride=1, downsample=None, norm_layer=None):
+    def __init__(self, input_chanels, output_chanels, stride=1, downsample=None, norm_Layer=None):
         super(Bottleneck, self).__init__()
-        if norm_layer is None:
+        if norm_Layer is None:
             norm_layer = nn.BatchNorm2d
         self.conv1 = conv1x1(input_chanels, output_chanels)
         self.bn1 = norm_layer(output_chanels)
@@ -88,7 +95,7 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classses=100, norm_layer=None):
+    def __init__(self, block, layers, num_classses=10, norm_layer=None):
         super(ResNet, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
@@ -106,7 +113,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
 
-        self.avgpool = nn.AdaptiveAvgPool1d2d((1, 1))
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classses)
 
     def _make_layer(self, block, output_chanels, blocks, stride=1):
@@ -115,7 +122,7 @@ class ResNet(nn.Module):
 
         if stride != 1 or self.input_chanels != output_chanels * block.expansion:
             downsample = nn.Sequential(
-                conv1x1(self.input_chanels, output_chanels * block.expansion, stride),
+                conv1x1(self.input_chanels, output_chanels * block.expansion, stride, 0),
                 norm_layer(output_chanels * block.expansion)
             )
 
@@ -124,7 +131,7 @@ class ResNet(nn.Module):
         self.input_chanels = output_chanels * block.expansion
 
         for _ in range(1, blocks):
-            layers.append(block(self.input_chanels, output_chanels, norm_layer=norm_layer))
+            layers.append(block(self.input_chanels, output_chanels, norm_Layer=norm_layer))
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -148,9 +155,17 @@ class ResNet(nn.Module):
 def resnet34():
     return ResNet(BasicBlock, [3, 4, 6, 3])
 
+#  [1,  1300] train_loss: 0.036  test_accuracy: 0.908
+
 
 def resnet152():
     return ResNet(Bottleneck, [3, 8, 36, 3])
 
+
+
+# input1 = torch.rand([32,3,244,244])
+model = resnet34() # 模式实例化
+print(model) # 看一下模型结构
+# output = model(input1)
 
 
